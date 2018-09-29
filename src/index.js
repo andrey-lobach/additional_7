42907e262fc9b48c8074fdf5d4969b2ef33b19c8
Andrei Lobach <andreylobach1997@gmail.com>
2018-09-29 14:41:40 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    for(let row = 0; row < matrix.length; row++){
+        for(let col = 0; col < matrix.length; col++){
+            if (matrix[row][col] == 0) {
+                for(let num = 1; num <= 9; num++){
+                    if (check(matrix,num,row,col)){
+                        matrix[row][col] = num;
+                        if (solveSudoku(matrix)) return matrix;
+                        matrix[row][col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+
+
+    function check(matrix, num, row, col) {
+        for (let i = 0; i < matrix.length; i++)
+            if (matrix[row][i] == num || matrix[i][col] == num) return false;
+        for(let i = row - row%3; i < row - row%3 + 3; i++)
+            for(let j = col - col%3; j < col - col%3 + 3; j++)
+                if (matrix[i][j] == num) return false;
+
+        return true;
+    }
 }
